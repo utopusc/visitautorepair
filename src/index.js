53308e24bbdce8c50ac8025d1205e2253db4af18
utopusc <87100762+utopusc@users.noreply.github.com>
2024-10-01 20:42:26 +0000

    analistci

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Analytics } from "@vercel/analytics/react"
 import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
     <App />
+    <Analytics />
   </>
 );
 
